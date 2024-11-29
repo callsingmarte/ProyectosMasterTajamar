f8661a5883abcc607e4064b1313c31e8f00e8206
CallsignMarte <56552217+callsingmarte@users.noreply.github.com>
2024-11-29 03:15:46 +0100

    Ejercicio Enrollment para Proyecto MultiplesTableCoreApp
    Añadida funcionalidad pedida para el ejercicio

---
+++ Core/MultiplesTablesCoreApp/MultiplesTablesCoreApp/Controllers/EnrollmentController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MultiplesTablesCoreApp.Data;
+using MultiplesTablesCoreApp.Models;
+using MultiplesTablesCoreApp.ViewModels;
+
+namespace MultiplesTablesCoreApp.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        SchoolDbContext _db;
+
+        public EnrollmentController(SchoolDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //id references courseId
+        public async Task<IActionResult> EnrollmentAdd(int id)
+        {
+            Course? course = await _db.Courses.FindAsync(id);
+
+            var studentsList = await _db.Students.Select(s => new
+            {
+                Id = s.StudentId,
+                Value = s.StudentName
+            }).ToListAsync();
+
+            EnrollmentAddViewModel vm = new EnrollmentAddViewModel();
+            vm.Course = course;
+            vm.StudentList = new SelectList(studentsList, "Id", "Value");
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EnrollmentAdd(EnrollmentAddViewModel vm)
+        {
+            Enrollment? studentEnrolled = await _db.Enrollments
+                .Where(e => e.CourseId == vm.Course!.CourseId && e.StudentId == vm.Student!.StudentId)
+                .Include(e => e.Student)
+                .Include(e => e.Course)
+                .FirstOrDefaultAsync();
+
+            if (studentEnrolled != null) {
+               return View("AlreadyEnrolled", studentEnrolled);
+            }
+            else
+            {
+                Course course = await _db.Courses.SingleAsync(c => c.CourseId == vm.Course!.CourseId);
+
+                if (course != null)
+                {
+                    Enrollment enrollment = new Enrollment
+                    {
+                        CourseId = vm.Course!.CourseId,
+                        StudentId = vm.Student!.StudentId
+                    };
+                    _db.Enrollments.Add(enrollment);
+                    await _db.SaveChangesAsync();
+
+                    course.SeatCapacity -= 1;
+                    _db.Update(course);
+                    await _db.SaveChangesAsync();
+                }
+
+                return RedirectToAction("AllCourse", "Course");
+            }
+        }
+
+        //id references course id
+        public async Task<IActionResult> EnrolledStudents(int id)
+        {
+            var enrolledStudents = await _db.Enrollments
+                .Where(e => e.CourseId == id)
+                .Include(e => e.Student)
+                .Include(e => e.Course)
+                .ToListAsync();
+
+            return View(enrolledStudents);
+        }
+    }
+}
---
+++ Core/MultiplesTablesCoreApp/MultiplesTablesCoreApp/ViewModels/EnrollmentAddViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MultiplesTablesCoreApp.Models;
+
+namespace MultiplesTablesCoreApp.ViewModels
+{
+    public class EnrollmentAddViewModel
+    {
+        public Course? Course { get; set; }
+        public Student? Student { get; set; }
+        public SelectList? StudentList { get; set; }
+    }
+}
