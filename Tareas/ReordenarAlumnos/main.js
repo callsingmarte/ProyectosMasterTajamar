696b2d639ed0307b5a487320312a60fdea102438
CallsignMarte <56552217+callsingmarte@users.noreply.github.com>
2024-12-13 03:45:39 +0100

    Avances en Ejercicio Ordenacion

--- Tareas/ReordenarAlumnos/main.js
+++ Tareas/ReordenarAlumnos/main.js
@@ -1,21 +1,24 @@
 class Student {
-    constructor(place, name){
+    constructor(place, name, image, colorClass){
         this.place = place,
-        this.name = name   
+        this.name = name,    
+        this.image = image
+        //Indica la pareja de alumnos bajo una misma clase
+        this.colorClass = colorClass
     }
 }
 
 let students = [
-    new Student(1, "David"),   
-    new Student(2, "Raul"),   
-    new Student(3, "Daniel"),   
-    new Student(4, "Robert"),   
-    new Student(5, "Daniel Salas"),   
-    new Student(6, "Ruben"),   
-    new Student(7, "Angel"),   
-    new Student(8, "Jorge"),   
-    new Student(9, "Nick"),   
-    new Student(10, "Martin"),   
+    new Student(1, "David", "Avatar1.jpg", "group1"),   
+    new Student(2, "Raul", "Avatar2.jpg", "group1"),   
+    new Student(3, "Daniel", "Avatar3.jpg", "group2"),   
+    new Student(4, "Robert", "Avatar4.jpg", "group2"),   
+    new Student(5, "Daniel Salas", "Avatar5.jpg", "group3"),   
+    new Student(6, "Ruben", "Avatar6.jpg", "group3"),   
+    new Student(7, "Angel", "Avatar7.jpg", "group4"),   
+    new Student(8, "Jorge", "Avatar8.jpg", "group4"),   
+    new Student(9, "Nick", "Avatar9.jpg", "group5"),   
+    new Student(10, "Martin", "Avatar10.jpg", "group5"),   
 ];
 
 let studentsColleaguesOrigin = [
@@ -32,8 +35,9 @@ let studentsColleaguesOrigin = [
 ]
 
 $(function (){
-    DisplayStudents();
     console.log("cargando estudiantes...");
+    DisplayStudents();
+    $("#fileInput").on("change", LoadFromFile);
 });
 
 function DisplayStudents(){
@@ -41,12 +45,17 @@ function DisplayStudents(){
     let row;
     for(let contador = 0; contador < students.length; contador++){
         if(contador+1 == 1 ||contador + 1 == 5 || contador + 1 == 9 ){
-            row = $("<div></div>").addClass("row");
+            row = $("<div></div>").addClass("row mb-3");
             $("#studentsContainer").append(row);
         }
         row.append(`
             <div class='col-3'>
-                <p>${students[contador].name}</p>
+                <div class="card" style="width: 18rem;">
+                    <img src="./images/${students[contador].image}" class="card-img-top"/>
+                    <div class="card-body ${students[contador].colorClass}">                
+                        <h5 class="card-title">${students[contador].name}</h5>
+                    </div>
+                </div>
             </div>
         `);
     }
@@ -99,6 +108,27 @@ function ReorderStudents() {
         ReorderStudents();
     }
 
+    //Cambio los grupos de color
+    students.forEach((student, index) => {
+        switch (true) {
+            case (index === 0 || index === 1):
+                student.colorClass = "group1";
+                break;
+            case (index === 2 || index === 3):
+                student.colorClass = "group2";
+                break;
+            case (index === 4 || index === 5):
+                student.colorClass = "group3";
+                break;
+            case (index === 6 || index === 7):
+                student.colorClass = "group4";
+                break;
+            case (index === 8 || index === 9):
+                student.colorClass = "group5";
+                break;
+        }
+    })
+
     DisplayStudents();
 }    
 function SaveOrderStudents() {
@@ -126,12 +156,47 @@ function SaveToFile() {
     const blob = new Blob([data], { type: "text/plain" });
 
     // Crear un enlace para la descarga
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = "studentsColleaguesOrigin.txt";
+    //Solo aceptamos un enlace el ultimo creado
+    $("#downloadLinkContainer").empty();
+    $("#downloadLinkContainer").append(
+        `<a id="downloadLink" href="${URL.createObjectURL(blob)}" download="OrdenacionEstudiantes.txt">
+        Descargar ordenacion estudiantes
+        <\a>`
+    );
 
-    // Simular clic en el enlace
-    link.click();
+    // Simular clic en el enlace    
+    $("#downloadLink")[0].click();
 
     console.log("Archivo guardado.");
 }
+
+function LoadFromFile(event) {
+    const file = event.target.files[0]; // Obtener el archivo seleccionado
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            try {
+                // Parsear los datos del archivo como JSON
+                const data = JSON.parse(e.target.result);
+
+                // Actualizar el array original
+                studentsColleaguesOrigin = data.map(item => ({
+                    student: new Student(item.student.place, item.student.name, item.student.image),
+                    colleague: new Student(item.colleague.place, item.colleague.name, item.colleague.image)
+                }));            
+                console.log("Datos cargados:", studentsColleaguesOrigin);
+
+                studentsColleaguesOrigin.forEach((studentColleague, index)=> {
+                    students[index] = studentColleague.student;
+                })
+
+                DisplayStudents();
+            } catch (error) {
+                console.error("Error al leer el archivo:", error);
+            }
+        };
+        reader.readAsText(file);
+    }
+}
+
