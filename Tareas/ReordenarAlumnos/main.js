4df632c4237118aec9d7ab8293dbf657eee620b0
CallsignMarte <56552217+callsingmarte@users.noreply.github.com>
2024-12-15 05:50:12 +0100

    Actualización Reordenar Alumnos
    Se implementan funciones en la tarea reordenar alumnos
    1.Comparador de ordenaciones
    2.Revelar y ocultar alumnos
    3.Eliminacion de console logs

--- Tareas/ReordenarAlumnos/main.js
+++ Tareas/ReordenarAlumnos/main.js
@@ -8,6 +8,10 @@ class Student {
     }
 }
 
+let isCoveringCard = true;
+let isComparing = false;
+let studentsOrigin = [];
+
 let students = [
     new Student(1, "David", "Avatar1.jpg", "group1"),   
     new Student(2, "Raul", "Avatar2.jpg", "group1"),   
@@ -36,7 +40,12 @@ let studentsColleaguesOrigin = [
 
 $(function (){
     console.log("cargando estudiantes...");
+    LoadStudentsOrigin();
     DisplayStudents();
+    $("#switchCoverCard").on("change", ToggleCoverCards)
+    //La ordenacion es igual a la original en la primera carga
+    //no tiene sentido ver la comparativa    
+    $("#btnCompare").prop("disabled", true);
     $("#fileInput").on("change", LoadFromFile);
 });
 
@@ -50,7 +59,7 @@ function DisplayStudents(){
         }
         row.append(`
             <div class='col-3'>
-                <div class="card" style="width: 18rem;">
+                <div class="card shadow position-relative" style="width: 18rem;">
                     <img src="./images/${students[contador].image}" class="card-img-top"/>
                     <div class="card-body ${students[contador].colorClass}">                
                         <h5 class="card-title">${students[contador].name}</h5>
@@ -79,28 +88,16 @@ function ReorderStudents() {
         let studentFoundIndex = students.findIndex( student => student.name === studentColleage.student.name);
 
         if(studentFoundIndex !== -1){                        
-            console.log("studentColleague primary to compare", studentColleage.student.name);
-            console.log("studentIndex", studentFoundIndex);
-            console.log("studentFound", students[studentFoundIndex]);
-            console.log("original colleague", studentColleage.colleague.name);    
             //Comprobar si el compañero es el mismo al reordenar
             if(studentFoundIndex % 2 == 1 && studentFoundIndex != 0){
-                console.log("new colleague", students[studentFoundIndex-1].name);    
                 if(students[studentFoundIndex-1].name === studentColleage.colleague.name){
-                    console.log("Repeated Colleague");
                     repetirReordenacion = true;
                 }
             }else if (students[studentFoundIndex + 1].name === studentColleage.colleague.name){
-                console.log("new colleague", students[studentFoundIndex+1].name);    
-                console.log("Repeated Colleague");
                 repetirReordenacion = true;
             }else if (studentFoundIndex == 0 && students[studentFoundIndex + 1].name === studentColleage.colleague.name){
-                console.log("new colleague", students[studentFoundIndex+1].name);    
-                console.log("Repeated Colleague");
                 repetirReordenacion = true;
             }
-        }else{
-            console.log("Student not found:", studentColleague.student.name);
         }
     })
 
@@ -127,10 +124,21 @@ function ReorderStudents() {
                 student.colorClass = "group5";
                 break;
         }
+        isComparing = false;
+        ChangeCompareBtnText();
     })
 
+    //Al haber un cambio de orden se puede comparar
+    $("#btnCompare").prop("disabled", false);
     DisplayStudents();
+
+    if(isCoveringCard){
+        CoverCards();   
+    }else {
+        UnCoverCards();
+    }
 }    
+
 function SaveOrderStudents() {
     students.forEach((student, index) => {        
         if(index == 0){
@@ -145,7 +153,16 @@ function SaveOrderStudents() {
         }
     })
     console.log("new students origin: ", studentsColleaguesOrigin)
+    LoadStudentsOrigin();
     SaveToFile();
+    if(isComparing){
+        DisplayStudents();
+    }
+    //Al guardar una nueva ordenacion esta coincide con la ordenacion actual
+    //por ello no se debe poder comparar
+    $("#btnCompare").prop("disabled", true);
+    isComparing = false;
+    ChangeCompareBtnText();    
 }
 
 function SaveToFile() {
@@ -182,16 +199,24 @@ function LoadFromFile(event) {
 
                 // Actualizar el array original
                 studentsColleaguesOrigin = data.map(item => ({
-                    student: new Student(item.student.place, item.student.name, item.student.image),
-                    colleague: new Student(item.colleague.place, item.colleague.name, item.colleague.image)
+                    student: new Student(item.student.place, item.student.name, item.student.image, item.student.colorClass),
+                    colleague: new Student(item.colleague.place, item.colleague.name, item.colleague.image, item.student.colorClass)
                 }));            
                 console.log("Datos cargados:", studentsColleaguesOrigin);
 
+                //Carga el listado de alumnos a comparar
+                LoadStudentsOrigin();
+
+                //Actualiza el listado de alumnos
                 studentsColleaguesOrigin.forEach((studentColleague, index)=> {
                     students[index] = studentColleague.student;
                 })
 
+                isComparing = false;
+                ChangeCompareBtnText();
                 DisplayStudents();
+                //Al cargar una nueva ordenacion esta coincide con la actual por ello no tiene sentido comparar
+                $("#btnCompare").prop("disabled", true);
             } catch (error) {
                 console.error("Error al leer el archivo:", error);
             }
@@ -200,3 +225,98 @@ function LoadFromFile(event) {
     }
 }
 
+function LoadStudentsOrigin(){    
+    studentsColleaguesOrigin.forEach((studentColleague, index)=> {
+        if(studentsOrigin.length == 0){
+            studentsOrigin.push(studentColleague.student);
+        }else{
+            studentsOrigin[index] = studentColleague.student;
+        }
+    })
+}
+
+function CompareStudentsOrder(){
+    if(isComparing){
+        DisplayStudents();
+        $("#btnCompare").text("Comparar");
+        isComparing = false;
+    }else{
+        $("#studentsContainer").empty(); // Limpiar contenedor
+        $("#studentsContainer").append(`<div class="row">
+            <div id="studentsCompare" class="col-6" style="border-right: 3px solid #000000">
+                <h2>Ordenación Original</h2>
+            </div>
+            <div id="students0rder" class="col-6">
+                <h2>Nueva Ordenación</h2>
+            </div>
+        </div>`);
+        DisplayStudentsComparison("studentsCompare", studentsOrigin);
+        DisplayStudentsComparison("students0rder", students);
+        isComparing = true;
+        $("#btnCompare").text("Listar estudiantes");    
+    }
+}
+
+function ChangeCompareBtnText(){
+    if(isComparing){
+        $("#btnCompare").text("Listar estudiantes");    
+    }else{
+        $("#btnCompare").text("Comparar");
+    }
+}
+
+function DisplayStudentsComparison(labelId, list){
+    let row;
+    for(let contador = 0; contador < list.length; contador++){
+        if(contador+1 == 1 ||contador + 1 == 5 || contador + 1 == 9 ){
+            row = $("<div></div>").addClass("row mb-3");
+            $(`#${labelId}`).append(row);
+        }
+        row.append(`
+            <div class='col-3'>
+                <div class="card shadow" style="width: 9rem;">
+                    <img src="./images/${list[contador].image}" class="card-img-top"/>
+                    <div class="card-body ${list[contador].colorClass}">                
+                        <h5 class="card-title">${list[contador].name}</h5>
+                    </div>
+                </div>
+            </div>
+        `);
+    }
+}
+
+function ToggleCoverCards() {
+    isCoveringCard = !isCoveringCard;
+    if(!isComparing){
+        if(isCoveringCard){
+            CoverCards();   
+        }else {
+            UnCoverCards();
+        }    
+    }
+}
+
+function UnCoverCards(){
+    $(".coverOverlay").each(function() {
+        $(this).remove();
+    })
+}
+
+function CoverCards(){
+    $(".card").each(function (){
+        $(this).append(`
+            <div class="coverOverlay position-absolute top-0 start-0 w-100 h-100 rounded" onclick="CardReveal(event)">
+                <img src='./images/CardCover.jpg' class='coverImg img-fluid rounded w-100 h-100' />
+            </div>
+        `);
+    });
+}
+
+function CardReveal(event){    
+    $(event.target).removeClass("coverImg");
+    $(event.target).addClass("cardReveal");
+    
+    setTimeout(function(){
+        $(event.target).remove();
+    }, 3000);
+}
