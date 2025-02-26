5843de96b503c2e163284dd5c3ff1a315f72e8c0
CallsignMarte <56552217+callsingmarte@users.noreply.github.com>
2025-02-26 01:24:37 +0100

    Autenticacion Azure

---
+++ Azure/JWT/NewWebApplicationLocal/NewWebApplicationLocal/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Abstractions;
+using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Tokens;
+using System.Text.Json;
+
+namespace NewWebApplicationLocal.Pages;
+
+[AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IDownstreamApi _downstreamWebApi;
+
+    public IndexModel(ILogger<IndexModel> logger, IDownstreamApi downstreamWebApi)
+    {
+        _logger = logger;
+        _downstreamWebApi = downstreamWebApi;
+    }
+
+    public async void OnGet()
+    {
+        using var response = await _downstreamWebApi.CallApiForUserAsync("DownstreamApi").ConfigureAwait(false);
+        if(response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            var apiResult = await response.Content.ReadFromJsonAsync<JsonDocument>().ConfigureAwait(false);
+            ViewData["ApiResult"] = JsonSerializer.Serialize(apiResult, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+        }
+        else
+        {
+            var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            throw new HttpRequestException($"Invalid status code in the HttpResponse : {response.StatusCode} : {error}");
+        }
+    }
+}
---
+++ Tareas/Azure/6 - Authentication/EjAzureAutenticacion/EjAzureAutenticacion/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+IEnumerable<string>? initialScopes =
+    builder.Configuration.GetSection("DownstreamApi:Scopes").Get<IEnumerable<string>>();
+
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, "AzureAd")
+    .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+    .AddDownstreamApi("DownstreamApi", builder.Configuration.GetSection("DownstreamApi"))
+    .AddInMemoryTokenCaches();
+
+// Add services to the container.
+builder.Services.AddRazorPages().AddMvcOptions(options =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+}).AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+
+
+app.Run();
