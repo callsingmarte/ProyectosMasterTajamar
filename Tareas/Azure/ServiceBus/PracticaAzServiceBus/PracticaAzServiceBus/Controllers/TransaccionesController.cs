330107197bf9c1999b977cc40ba40294412b40ac
Martin Sanchez <56552217+callsingmarte@users.noreply.github.com>
2025-03-24 20:52:41 +0100

    Avances Practica ServiceBus

---
+++ Tareas/Azure/ServiceBus/PracticaAzServiceBus/PracticaAzServiceBus/Controllers/TransaccionesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaAzServiceBus.Controllers
+{
+    public class TransaccionesController : Controller
+    {
+        private readonly string _queueName;
+        private readonly string _topicName;
+        private readonly string _subscriptionName;
+        private readonly string _connectionString;
+
+        TransaccionesController(IConfiguration configuration)
+        {
+            _connectionString = configuration["AzureServiceBus:ConnectionString"] != "" ? configuration["AzureServiceBus:ConnectionString"]! : Environment.GetEnvironmentVariable("AzureServiceBusConnectionString")!;
+            _queueName = configuration["AzureServiceBus:QueueName"]!;
+            _topicName = configuration["AzureServiceBus:TopicName"]!;
+            _subscriptionName = configuration["AzureServiceBus:SubscriptionName"]!;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
