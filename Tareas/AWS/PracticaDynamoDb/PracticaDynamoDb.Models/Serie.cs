dc3e183a0cf0e16e8258d4694a245ab49de5f60c
Martin Sanchez <56552217+callsingmarte@users.noreply.github.com>
2025-04-24 20:53:46 +0200

    Inicio Practica DynamoDb

---
+++ Tareas/AWS/PracticaDynamoDb/PracticaDynamoDb.Models/Serie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaDynamoDb.Models
+{
+    public class Serie
+    {
+        public string SerieId { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public int Temporadas { get; set; }
+        public string DisponibleEn { get; set; } //Lo puedes hacer enum
+    }
+}
